62f32a3c6d2c9183ed0eb4f84dc498d1e49ce8ba
Chad Fennell <fenne035@umn.edu>
2014-03-13 10:55:28 -0500

    add import log to production

--- app/models/concerns/import_log.rb
+++ app/models/concerns/import_log.rb
@@ -3,7 +3,7 @@ module ImportLog
   def import_log
     if Rails.env.test?
       @@import_log ||= Logger.new("#{Rails.root}/log/import-test.log")
-    elsif Rails.env.development?
+    elsif Rails.env.development? || Rails.env.production?
       @@import_log ||= Logger.new("#{Rails.root}/log/import.log")
     end
   end
